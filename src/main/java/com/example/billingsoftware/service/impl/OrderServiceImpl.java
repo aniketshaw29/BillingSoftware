e3d1d512af868cf5720034fee0e862d8e70659a5
aniketshaw29 <aniketshawcoding@gmail.com>
2022-06-20 13:38:52 +0530

    Added service, impl

---
+++ src/main/java/com/example/billingsoftware/service/impl/OrderServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.billingsoftware.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.billingsoftware.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+}
---
+++ src/main/java/com/example/billingsoftware/service/impl/ProductServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.billingsoftware.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.billingsoftware.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+}

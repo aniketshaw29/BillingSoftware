61855ec1fa5d1ed3c58f14daabba0fe6cb38f931
aniketshaw29 <aniketshawcoding@gmail.com>
2022-06-23 12:46:02 +0530

    Added System properties file and added the core config method in Application class

--- src/main/java/com/example/billingsoftware/BillingsoftwareApplication.java
+++ src/main/java/com/example/billingsoftware/BillingsoftwareApplication.java
@@ -2,6 +2,11 @@ package com.example.billingsoftware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BillingsoftwareApplication {
@@ -9,5 +14,19 @@ public class BillingsoftwareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BillingsoftwareApplication.class, args);
 	}
+	@Configuration
+	public class CorsConfig {
 
+	    @SuppressWarnings("deprecation")
+		@Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurerAdapter() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+	                        .allowedHeaders("*");
+	            }
+	        };
+	    }
+	}
 }
